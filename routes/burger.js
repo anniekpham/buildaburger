91c0121b8c2d4a4ed764a7cfc7b462eef791c394
Annie Pham <anniekpham@yahoo.com>
2019-05-18 14:23:56 -0700

    added routes

---
+++ routes/burger.js
@@ -0,0 +1,7 @@
+const {Router} = require('express')
+const router = Router()
+
+router.get('/', (req, res) => {
+    console.log('yay')
+})
+module.exports = router
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const { join } = require('path')
 const exphbs = require("express-handlebars")
 const app = express()
+const routes = require('./routes')
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
+app.use(routes)
 
 app.listen(3000)
