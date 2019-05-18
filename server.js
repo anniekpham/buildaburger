2e42cb857e8719f354baf0b0f3eb0c51cbf7d1c5
Annie Pham <anniekpham@yahoo.com>
2019-05-18 14:05:08 -0700

    server init add handlebar

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('yay')
+const express = require('express')
+const { join } = require('path')
+const exphbs = require("express-handlebars")
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+app.listen(3000)
