b8ee805cee779179a534d839756bf354ee23a77a
Annie Pham <anniekpham@yahoo.com>
2019-05-21 19:45:51 -0700

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,23 @@
 const { createConnection } = require('mysql2')
 
-module.exports = createConnection({
-  host: `${process.env.jawdb}`,
-  port: null,
-  user: `${process.env.db_acc}`,
-  password: `${process.env.db_pw}`,
-  database: 'burgers_db'
-})
+// module.exports = createConnection({
+//   host: `${process.env.JAWS_DB}`,
+//   port: null,
+//   user: `${process.env.db_acc}`,
+//   password: `${process.env.db_pw}`,
+//   database: 'burgers_db'
+// })
+let connection
+if (process.env.JAWS_DB) {
+  connection = createConnection(process.env.JAWS_DB)
+} else {
+  connection = createConnection({
+    host: 'localhost',
+    port: null,
+    user: process.env.db_acc,
+    password: process.env.db_pw,
+    database: 'burgers_db'
+  })
+}
+
+module.exports = connection
